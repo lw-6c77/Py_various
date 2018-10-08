993db1a076235384be7f28c5828effda125dba80
lw-6c77 <lukas.wilk@gmail.com>
2018-10-08 10:21:16 +0100

    Revert "update"
    This reverts commit f05ba85e1b205d55126325bb86a1a0a52bd00382.

---
+++ addMissingJavaPackageName.py
@@ -0,0 +1,12 @@
+import os
+
+for dir_path, directories, files in os.walk(os.getcwd()):
+    for i in files:
+        if (i.endswith(".java")):
+            file_path = os.path.join(dir_path, i)
+            with open(file_path, "r") as missing_package:
+                temp = missing_package.read()
+            with open(file_path, 'w') as to_be_packaged:
+                to_be_packaged.write("package " + os.path.basename(dir_path) + ";" + "\n\n" + temp)
+            to_be_packaged.close()
+            
